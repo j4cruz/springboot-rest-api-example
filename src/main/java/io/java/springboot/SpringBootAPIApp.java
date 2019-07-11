6d2aa0b0b888a7e834e0d0a5176cb605a316afb1
Taran Chauhan <taran.chauhan@hotmail.co.uk>
2019-07-11 18:21:32 +0100

    Spring Boot Application main method bootstrap launch

---
+++ src/main/java/io/java/springboot/SpringBootAPIApp.java
@@ -0,0 +1,13 @@
+package io.java.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAPIApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAPIApp.class,  args);
+	}
+
+}
